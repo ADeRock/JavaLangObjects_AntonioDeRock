fb86f823a086f64391f32ceaa9c020029816a462
ADeRock <aderock19@scienceleadership.org>
2018-10-03 08:24:33 -0400

    Made code clearer

--- src/AntonioDeRock.java
+++ src/AntonioDeRock.java
@@ -2,22 +2,20 @@ package org.sla;
 
  class AntonioDeRock {
      public static void main(String[] args) {
-         System.out.println("First simplify in the parenthesis, (4.5 * 2.2)^2 / 5 + 3!");
-         System.out.println("Raise 9.9 to the 2nd power, (9.9)^2 / 5 + 3!");
-         System.out.println("Divide by 5, (98.01) / 5 + 3!");
-         System.out.println("Add 3, 19.602 + 3!");
-         System.out.println("22.602!");
-
          System.out.println(" ");
 
          double A = Math.pow(4.5 * 2.2, 2);
+         System.out.println("First simplify in the parenthesis, (4.5 * 2.2)^2 / 5 + 3!");
+         System.out.println("Raise 9.9 to the 2nd power, (9.9)^2 / 5 + 3!");
          System.out.println(A);
 
          double B = A / 5;
+         System.out.println("Divide by 5, (98.01) / 5 + 3!");
          System.out.println(B);
 
-         double C = B + 3;
-         System.out.println(C);
 
+         double C = B + 3;
+         System.out.println("Add 3, 19.602 + 3!");
+         System.out.println(C + "!");
      }
 }
