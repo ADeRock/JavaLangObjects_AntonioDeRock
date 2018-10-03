bc053f37a9e11439d9c2e8eb09f32e280971602b
ADeRock <aderock19@scienceleadership.org>
2018-10-03 08:56:26 -0400

    Made code clearer. Made changes.. Formatting.

--- src/AntonioDeRock.java
+++ src/AntonioDeRock.java
@@ -12,21 +12,20 @@ package org.sla;
          System.out.println(" ");
 
          double A = Math.pow(4.5 * 2.2, 2);
-         System.out.println("First simplify in the parenthesis, (" + a + " * " + b + ")^2 / " + c + " + " + d + "!");
-        double temp = a * b;
-         System.out.println("Raise " + temp + " to the 2nd power, (" + temp + ")^2 / 5 + 3!");
-         System.out.println(A);
+            System.out.println("First simplify in the parenthesis, (" + a + " * " + b + ")^2 / " + c + " + " + d + "!");
+             double temp = a * b;
+             System.out.println("Raise " + temp + " to the 2nd power, (" + temp + ")^2 / 5 + 3!");
+             System.out.println(A);
 
          double B = A / 5;
-         double tem = c;
-         System.out.println("Divide by " + tem + ", (98.01) / " + tem + " + 3!");
-         System.out.println(B);
+             double tem = c;
+             System.out.println("Divide by " + tem + ", (98.01) / " + tem + " + 3!");
+            System.out.println(B);
 
 
          double C = B + 3;
-         double te = d;
-         System.out.println("Add " + te + ", 19.602 + " + te + " !");
-         System.out.println(C + "!");
-
+             double te = d;
+             System.out.println("Add " + te + ", 19.602 + " + te + " !");
+            System.out.println(C + "!");
      }
 }
